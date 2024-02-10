ee8e13b843066e3dfb459743a1a13dc5f7e98911
kbhusal7 <153198634+kbhusal7@users.noreply.github.com>
2024-02-10 14:55:28 +0545

    Add files via upload
    pushed at first

---
+++ museum_nepal/footer.php
@@ -0,0 +1,8 @@
+  <div class="footer1">
+      <div> <img width="100" hight="100" src="img/logo.png" alt=""></div>
+      <div>
+          <center>
+              <p> ehhehehehe</p>
+          </center>
+      </div>
+  </div>
